54aee1cb9008fd36342b731ec430ab5db8ff0583
andrea-berling <berlingieri.andrea@gmail.com>
2017-07-15 20:04:51 +0200

    Map is not a subclass of Matrix anymore
    Map class is not a subclass of Matrix anymore: it now has a Matrix
    member called grid, which keeps the grid of the map

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,22 +1,22 @@
 #include "../include/Map.h"
 
-Map::Map() : Matrix()
+Map::Map():grid(0,0),width(0),height(0)
 {}
 // Defualt constructor
 
-Map::Map(int width, int height) : Matrix(height, width), width(width), height(height)
+Map::Map(int width, int height) : grid(height, width), width(width), height(height)
 {}
 // Creates a new map with the given # of rows and columns
 
 tile_t& Map::operator () (int x, int y)
 {
-    return matrix[y][x];
+    return grid(y,x);
 }
 // Returns a reference variable to Map(x,y)
 
 tile_t& Map::operator () (point p)
 {
-    return matrix[p.y][p.x];
+    return grid(p.y,p.x);
 }
 // Returns a reference variable to Map(x,y)
 
