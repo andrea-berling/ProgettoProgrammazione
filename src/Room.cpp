d9805de4b58c261e16f3b069ec0bf8abadd1c7af
andrea-berling <andrea-berling@users.noreply.github.com>
2017-07-15 18:02:00 +0200

    Replaced pointers with references
    Replaced some pointer references with pass-by-reference parameters
    
    Added a copy constructor for the list
    
    Removed the DEBUG methods and macros from newtest.cpp

--- src/Room.cpp
+++ src/Room.cpp
@@ -13,35 +13,35 @@ Room::Room(Room& lhs) : p(lhs.p), width(lhs.width), height(lhs.height)
 Room::Room(point position, int width, int height) : p(position), width(width), height(height)
 {}
 
-void Room::place(Map* M)
+void Room::place(Map& M)
 {
     for(int i = p.x; i < p.x + width; i++)
         for(int j = p.y; j < p.y + height; j++)
-            (*M)(i,j) = PAVEMENT;
+            (M)(i,j) = PAVEMENT;
     for(int i = p.x - 1; i <= p.x + width; i++)
     {
-        (*M)(i,p.y - 1) = ROOM_BORDER;
-        (*M)(i,p.y + height) = ROOM_BORDER;
+        (M)(i,p.y - 1) = ROOM_BORDER;
+        (M)(i,p.y + height) = ROOM_BORDER;
     }
     for(int i = p.y - 1; i <= p.y + height; i++)
     {
-        (*M)(p.x - 1,i) = ROOM_BORDER;
-        (*M)(p.x + width,i) = ROOM_BORDER;
+        (M)(p.x - 1,i) = ROOM_BORDER;
+        (M)(p.x + width,i) = ROOM_BORDER;
     }
 }
 
-bool Room::overlaps(Map* M)
+bool Room::overlaps(Map& M)
 {
-    if((*M)(p.x,p.y) == PAVEMENT || (*M)(p.x,p.y) == ROOM_BORDER)
+    if((M)(p.x,p.y) == PAVEMENT || (M)(p.x,p.y) == ROOM_BORDER)
         return true;
     else
     {
         for(int i = p.x; i < p.x + width; i++)
-            if((*M)(i,p.y) == PAVEMENT || (*M)(i,p.y + height - 1) == PAVEMENT || (*M)(i,p.y) == ROOM_BORDER || (*M)(i,p.y + height - 1) == ROOM_BORDER)
+            if((M)(i,p.y) == PAVEMENT || (M)(i,p.y + height - 1) == PAVEMENT || (M)(i,p.y) == ROOM_BORDER || (M)(i,p.y + height - 1) == ROOM_BORDER)
                 return true;
 
         for(int i = p.y; i < p.y + height; i++)
-            if((*M)(p.x,i) == PAVEMENT || (*M)(p.x + width - 1,i) == PAVEMENT || (*M)(p.x,i) == ROOM_BORDER || (*M)(p.x + width - 1,i) == ROOM_BORDER)
+            if((M)(p.x,i) == PAVEMENT || (M)(p.x + width - 1,i) == PAVEMENT || (M)(p.x,i) == ROOM_BORDER || (M)(p.x + width - 1,i) == ROOM_BORDER)
                 return true;
 
         return false;
@@ -69,7 +69,7 @@ Room generateRoom(int w, int h,int seed)
 
 }
 
-point Room::pickAPointAround(Map* M)
+point Room::pickAPointAround(Map& M)
 {
     using namespace std;    
 
@@ -82,7 +82,7 @@ point Room::pickAPointAround(Map* M)
         q.x = p.x - 1;
         q.y = rand(p.y, p.y + height - 1);
         i = 0;
-//        while((*M)(q.x,q.y) == PAVEMENT && i < height)
+//        while((M)(q.x,q.y) == PAVEMENT && i < height)
 //        {
 //            q.y = p.y + i % height;
 //            i++;
@@ -97,7 +97,7 @@ point Room::pickAPointAround(Map* M)
         q.x = rand(p.x, p.x + width - 1);
         points.push_back(q);
         i = 0;
-//        while((*M)(q.x,q.y) == PAVEMENT && i < width)
+//        while((M)(q.x,q.y) == PAVEMENT && i < width)
 //        {
 //            q.x = p.x + i % width;
 //            i++;
@@ -106,12 +106,12 @@ point Room::pickAPointAround(Map* M)
         points.push_back(q);
     }
 
-    if(p.x + width < M->getWidth())
+    if(p.x + width < M.getWidth())
     {
         q.x = p.x + width;
         q.y = rand(p.y, p.y + height - 1);
         points.push_back(q);
-//        while((*M)(q.x,q.y) == PAVEMENT && i < height)
+//        while((M)(q.x,q.y) == PAVEMENT && i < height)
 //        {
 //            q.y = p.y + i % height;
 //            i++;
@@ -120,12 +120,12 @@ point Room::pickAPointAround(Map* M)
         points.push_back(q);
     }
 
-    if(p.y + height < M->getHeight())
+    if(p.y + height < M.getHeight())
     {
         q.y = p.y + height;
         q.x = rand(p.x, p.x + width - 1);
         points.push_back(q);
-//        while((*M)(q.x,q.y) == PAVEMENT && i < width)
+//        while((M)(q.x,q.y) == PAVEMENT && i < width)
 //        {
 //            q.x = p.x + i % width;
 //            i++;
