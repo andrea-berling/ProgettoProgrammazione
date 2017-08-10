ab66fcaa889a9b5860f38869fa58406109df72e3
andrea-berling <andrea-berling@users.noreply.github.com>
2017-08-10 11:18:18 +0200

    The pick methods, which is now useless, has been removed

--- include/utility.hpp
+++ include/utility.hpp
@@ -2,9 +2,6 @@
 #define UTILITY_H
 #include <cstdlib>
 #include <ctime>
-#include <vector>
-
-using namespace std;
 
 // It returns a random integer between first and second (or viceversa if second < first)
 // Precondition: the function srand(time(NULL)) should be called before using this method
@@ -14,15 +11,6 @@ inline int rand(int first, int second)
         return first + rand() % (second - first + 1);
     else
         return second + rand() % (first - second + 1);
-}
-
-// Given a vector of elements, it picks one randomly and returns it
-template<typename T> 
-T pick(vector<T> args)
-{
-    int n = args.size() - 1;
-    int index = rand(0,n);
-    return args[index];
 }
 
 #endif
