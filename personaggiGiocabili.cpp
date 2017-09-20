c35c39c072d5613accdbc926ab49d83e0193b57c
Zacomo <giacomo.puligheddu@hotmail.it>
2017-09-20 14:57:31 +0200

    Introduzione degli insiemi e Item come classe.
    Ho rimosso le liste e le ho sostituite con gli insiemi.
    Ho cambiato gli item: adesso non sono più strutture (struct) ma sono
    degli oggetti della classe Item.
    Ho aggiunto il metodo LVLup che modifica le stats quando si livella.

---
+++ personaggiGiocabili.cpp
@@ -0,0 +1,178 @@
+//
+// Created by zacomo on 19/09/17.
+//
+//
+// Created by zacomo
+//
+
+
+#include <unordered_set>
+
+#include "personaggiGiocabili.h"
+
+
+    PlayableCharacter::PlayableCharacter(double LP, double MP, double DEF, double ATK, std::string Name):Inventory(47){
+        this->LP = LP;
+        this->MP = MP;
+        this->DEF = DEF;
+        this->ATK = ATK;
+        this->POS.X = 0;
+        this->POS.Y = 0;
+        this->LV = 0;
+        this->Coins = 0;
+        this->Name = Name;
+        for (int i = 0; i<6; i++){
+            equipment [i] = Item();      //Inizializza equipment
+        }
+
+    }
+
+    double PlayableCharacter::getLP(){
+        return this->LP;
+    }
+
+    double PlayableCharacter::getMP(){
+        return this->MP;
+    }
+
+    double PlayableCharacter::getDEF(){
+        return this->DEF;
+    }
+
+    double PlayableCharacter::getATK(){
+        return this->ATK;
+    }
+
+    Position PlayableCharacter::getPosition() {
+        return this->POS;
+    }
+
+    int PlayableCharacter::getLV(){
+        return this->LV;
+    }
+
+    int PlayableCharacter::getCoins(){
+        return this->Coins;
+    }
+
+    void PlayableCharacter::setLP(double LP){
+        this->LP = LP;
+    }
+
+    void PlayableCharacter::setMP(double MP){
+        this->MP = MP;
+    }
+
+    void PlayableCharacter::setDEF(double DEF){
+        this->DEF = DEF;
+    }
+
+    void PlayableCharacter::setATK(double ATK){
+        this->ATK = ATK;
+    }
+
+    void PlayableCharacter::setPosition(double x, double y){
+        this->POS.X = x;
+        this->POS.Y = y;
+    }
+
+/*  void PlayableCharacter::setLV(int LV){
+        this->LV = LV;
+    }
+*/
+    void PlayableCharacter::setCoins(int Coins){
+        this->Coins = Coins;
+    }
+
+    std::string PlayableCharacter::getName(){
+        return this->Name;
+    }
+
+    bool PlayableCharacter::useConsumable(Item sbobba){
+        if (sbobba.gettype() == 6){
+            LP = LP + sbobba.getLP();
+            MP = MP + sbobba.getMP();
+            DEF = DEF + sbobba.getDEF();
+            ATK = ATK + sbobba.getATK();
+            Inventory.erase(Inventory.find(sbobba));           //consumabile utilizzabile una sola volta
+            return true;
+        }
+        return false;
+    }
+
+    bool PlayableCharacter::equip(Item ferraglia){                       //
+        if ((ferraglia.gettype() > -1 && ferraglia.gettype() < 6) && (equipment[ferraglia.gettype()] == Item())){
+
+            equipment[ferraglia.gettype()] = ferraglia;
+            LP = LP + ferraglia.getLP();
+            MP = MP + ferraglia.getMP();
+            DEF = DEF + ferraglia.getDEF();
+            ATK = ATK + ferraglia.getATK();
+            return true;
+        }
+        return false;
+    }
+
+    bool PlayableCharacter::unequip(Item ferraglia){
+        if (equipment[ferraglia.gettype()] != Item()){
+            LP = LP - ferraglia.getLP();
+            MP = MP - ferraglia.getMP();
+            DEF = DEF - ferraglia.getDEF();
+            ATK = ATK - ferraglia.getATK();
+            equipment[ferraglia.gettype()] = Item(); //Rimuove l'oggetto dall'equip.
+            return true;
+        }
+
+        return false;
+    }
+
+
+    bool PlayableCharacter::getItem(Item thing){
+        bool done = false;
+        if (Inventory.size() < 20) {     //20 è la dimensione dell'inventario
+            Inventory.insert(thing);
+            done = true;
+        }
+        return done;
+    }
+
+    bool PlayableCharacter::dropItem(Item thing){
+        bool done = false;
+        if (!Inventory.empty()) {
+
+            Inventory.erase(thing);
+            done = true;
+        }
+        return done;
+    }
+
+    void PlayableCharacter::LVLup() {
+        this->LV++;
+        double atk = 0, def = 0, lp = 0, mp = 0;
+        if (this->Name == "Gaudenzio"){
+            atk = 2;
+            def = 3;
+            lp = 2;
+    }
+        if (this->Name == "Peppino"){
+            atk = 1;
+            def = 1;
+            lp = 1;
+            mp = 3;
+        }
+
+        if (this->Name == "Badore"){
+            atk = 3;
+            mp = 1;
+            lp = 1;
+        }
+        this->ATK += atk;
+        this->DEF += def;
+        this->MP  += mp ;
+        this->LP  += lp ;
+
+};
+
+
+
+
--- personaggiGiocabili.h
+++ personaggiGiocabili.h
@@ -2,7 +2,10 @@
 // Created by zacomo
 //
 
-#include "list.h"
+
+#include <unordered_set>
+#include <string>
+#include "Item.h"
 
 struct Position{
     double X, Y;
@@ -13,157 +16,55 @@ private:
     double LP, MP, DEF, ATK;
     int LV, Coins;
     Position POS;
-    string  Name, Description;
-    listptr Inventory;
-    listptr Equipment;
-    //int InvSize; dimensioni Inventario
+    std::string Name;
+    std::unordered_multiset<Item> Inventory;
+    Item equipment [6];
 
 public:
 
-    PlayableCharacter(double LP, double MP, double DEF, double ATK, string Name, string Description){
-        this->LP = LP;
-        this->MP = MP;
-        this->DEF = DEF;
-        this->ATK = ATK;
-        this->POS.X=0;
-        this->POS.Y=0;
-        this->LV = 0;
-        this->Coins = 0;
-        this->Name = Name;
-        this->Description = Description;
-        Inventory = NULL;
-        Equipment = NULL;
-    }
+    PlayableCharacter(double LP, double MP, double DEF, double ATK, std::string Name):Inventory(47);
 
-    double getLP(){
-        return LP;
-    }
+    double getLP();
 
-    double getMP(){
-        return MP;
-    }
+    double getMP();
 
-    double getDEF(){
-        return DEF;
-    }
+    double getDEF();
 
-    double getATK(){
-        return ATK;
-    }
+    double getATK();
 
-    Position getPosition() {
-        return this->POS;
-    }
+    Position getPosition();
 
-    int getLV(){
-        return LV;
-    }
+    int getLV();
 
-    int getCoins(){
-        return Coins;
-    }
+    int getCoins();
 
-    void setLP(double LP){
-        this->LP = LP;
-    }
+    void setLP(double LP);
 
-    void setMP(double MP){
-        this->MP = MP;
-    }
+    void setMP(double MP);
 
-    void setDEF(double DEF){
-        this->DEF = DEF;
-    }
+    void setDEF(double DEF);
 
-    void setATK(double ATK){
-        this->ATK = ATK;
-    }
+    void setATK(double ATK);
 
-    void setPosition(double x, double y){
-        this->POS.X = x;
-        this->POS.Y = y;
-    }
+    void setPosition(double x, double y);
 
-    void setLV(int LV){
-        this->LV = LV;
-    }
+    //void setLV(int LV);
 
-    void setCoins(int Coins){
-        this->Coins = Coins;
-    }
+    void setCoins(int Coins);
 
-    string getName(){
-        return Name;
-    }
+    std::string getName();
 
-    string getDescription(){
-        return Description;
-    }
+    bool useConsumable(Item sbobba);
 
-    listptr getEquipment(){
-        return Equipment;
-    }
+    bool equip(Item ferraglia);
 
-    listptr getInventory(){
-        return Inventory;
-    }
+    bool unequip(Item ferraglia);
 
-    string useConsumable(Item thing){ //implementabile anche con descrizione
-        listptr tmpptr = getPosition(thing, Inventory);
-        if ((!is_in_type(thing.type, Inventory)) && (tmpptr->thing.consumable)){
-            LP = LP + tmpptr->thing.LP;
-            MP = MP + tmpptr->thing.MP;
-            DEF = DEF + tmpptr->thing.DEF;
-            ATK = ATK + tmpptr->thing.DEF;
-            ListRemove(thing, Inventory);           //consumabile utilizzabile una sola volta
-            return thing.description + " usato!";
+    bool getItem(Item thing);
 
-        }
-        return "Non è un consumabile o non è nel tuo inventario!";
-    }
+    bool dropItem(Item thing);
 
-    string equipItem(Item thing){
-        if (!is_in_type(thing.type, Equipment)){
-
-            listptr tmpptr = getPosition(thing, Inventory);
-            tmpptr->equipped = true;
-            LP = LP + tmpptr->thing.LP;
-            MP = MP + tmpptr->thing.MP;
-            DEF = DEF + tmpptr->thing.DEF;
-            ATK = ATK + tmpptr->thing.ATK;
-            HeadInsert(thing, Equipment);
-            return "Oggetto equipaggiato!";
-        }
-        return "Hai già equipaggiato un oggetto dello stesso tipo!";
-
-    }
-
-    string unequipItem(Item thing){
-        listptr tmpptr = getPosition(thing, Inventory);
-        if (is_in(thing, Equipment)){
-            tmpptr->equipped = false;
-            LP = LP - tmpptr->thing.LP;
-            MP = MP - tmpptr->thing.MP;
-            DEF = DEF - tmpptr->thing.DEF;
-            ATK = ATK - tmpptr->thing.ATK;
-            ListRemove(thing, Equipment);
-            return "Oggetto riposto nell'inventario!";
-        }
-
-        return "L'oggetto non è stato equipaggiato!";
-
-    }
-
-    string getItem(Item thing){
-        HeadInsert(thing, Inventory);
-        Inventory->equipped = false; //un oggetto appena raccolto non è equipaggiato
-        return thing.description + " è stato raccolto!";
-    }
-
-    string dropItem(Item thing){     //Eventualmente posso implementarlo con la descrizione
-        ListRemove(thing, Inventory); //dell'Item.
-        return thing.description + " è stato lasciato!";
-    }
+    void LVLup();
 };
 
 
