d1efa39284a1291482844bd839afbebb58bdc6d8
andrea-berling <andrea-berling@users.noreply.github.com>
2017-09-20 15:38:24 +0200

    Wrote a function that creates an arrangement of n elements chosen between two numbers

--- include/utility.hpp
+++ include/utility.hpp
@@ -13,4 +13,27 @@ inline int rand(int first, int second)
         return second + rand() % (first - second + 1);
 }
 
+// Given an array, its dimension, a range of numbers and a number n, it puts an ordered arrangement of n numbers taken
+// from the given range
+void generateArrangement(int array, int first, int last, int n)
+{
+    int d = last - first + 1;
+    int i = 0,j=0;
+    int numbers[d];
+    int index;
+
+    for(int i = 0; i < d; i++)
+        numbers[i] = i;
+
+
+    while(i < n)
+    {
+        index = rand(i,d-1);
+        array[j] = numbers[index];
+        swap(numbers[i],numbers[index]);
+        i++;
+        j++;
+    }
+}
+
 #endif
