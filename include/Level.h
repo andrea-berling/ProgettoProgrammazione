2089432f6182a64b922f98547a77af017adae3bc
andrea-berling <berlingieri.andrea@gmail.com>
2017-09-22 11:11:33 +0200

    Added printMap and placeCharacter
    Added method to print the map and to place the main character

--- include/Level.h
+++ include/Level.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <string>
 #include "Graph.h"
+#include "personaggiGiocabili.h"
 #include "Monster.h"
 #include "Item.h"
 
@@ -18,7 +19,13 @@ class Level
 
     public:
 
-        Level(int width,int height,int rooms, int _monsters, int _items);
+        Level(int level,int width,int height,int rooms, int _monsters, int _items);
         // Given a width, a height and a  number of rooms, it creates a new level
+
+        void printMap();
+        // Prints the map, the monsters and the items
+
+        void placeCharacter(PlayableCharacter& player);
+        // Given a main character, places it in a random room in the map
 };
 #endif
--- src/Level.cpp
+++ src/Level.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Level::Level(int width,int height,int rooms, int _monsters, int _items):map(width,height),monsters(47),items(47)
+Level::Level(int level, int width, int height, int rooms, int _monsters, int _items):level(level),map(width,height),monsters(47),items(47)
 {
     unordered_set<Point> spots;
     map.generate(rooms);
@@ -17,4 +17,34 @@ Level::Level(int width,int height,int rooms, int _monsters, int _items):map(widt
     {
         // Spawn a monster
     }
+}
+
+void Level::printMap()
+{
+    clear();
+    move(0,0);
+    for(int i = 0; i < map.getHeight(); i++)
+    {
+        for (int j = 0; j < map.getWidth(); j++)
+        {
+            addch(map(j,i).isVisible() ? map(j,i).getSymbol() : ' ');
+        }
+        addch('\n');
+    }
+    for(Monster m : monster)
+    {
+        Point position = m.getPosition();
+        mvaddch(position.x,position.y,m.getSymbol());
+    }
+    for(Item i : items)
+    {
+        Point position = i.getPosition();
+        mvaddch(position.x,position.y,i.getSymbol());
+    }
+    refresh();
+}
+
+void placeCharacter(PlayableCharacter& player)
+{
+    map.placeCharacter(player);
 }
