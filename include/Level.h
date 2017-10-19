0b6c09201bdf52ed72a7505d09f2ab0fea949a54
andrea-berling <berlingieri.andrea@gmail.com>
2017-10-19 13:12:57 +0200

    Modified the printMap method
    Now the printMap method takes just the player position.

--- include/Level.h
+++ include/Level.h
@@ -27,7 +27,7 @@ class Level
         Level(int level,int width,int height,int rooms, int _monsters, int _items, PlayableCharacter& pg);
         // Given a width, a height and a number of rooms, monsters and items, it creates a new level
 
-        void printMap(PlayableCharacter& player, Window& mapWindow);
+        void printMap(Point playerPos, Window& mapWindow);
         // Prints the map, the monsters, the items and the player
 
         void placeCharacter(PlayableCharacter& player, int playerPosition);
