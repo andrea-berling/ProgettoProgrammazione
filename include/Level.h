4c26228149faad84bfe581979621475831231ee7
andrea-berling <andrea-berling@users.noreply.github.com>
2017-10-26 16:25:00 +0200

    Refactoring and polishing
    Did some refactoring of the code: specifically i broke down the handleTurn method into a number of submethods with a
    specific funtcion each.
    
    Added some enum definition to make return statuses clearer.
    
    Added a LevelConfig struct to have a lower number of arguments in the Level constructor.
    
    Modified the involved code to reflect the new changes.
    
    Bugfixes of the placeStairs and placeCharacter methods: now the stairs won't be in the way of an exit.
    
    Made the writeInfo method a method member of the Level class.

--- include/Level.h
+++ include/Level.h
@@ -10,6 +10,18 @@
 #include "Item.h"
 #include "Window.h"
 
+enum status_t {WIN, LOSS, UP, DOWN, QUIT, DEFAULT};
+
+enum pos_pref_t {RANDOM, UPSTAIRS, DOWNSTAIRS};
+
+struct LevelConfig
+{
+    int n,width,height,rooms,monsters,items;
+
+    void newLevel();
+    // Changes the configuration for a new level
+};
+
 class Level
 {
     private:
@@ -24,19 +36,19 @@ class Level
 
     public:
 
-        Level(int level,int width,int height,int rooms, int _monsters, int _items, PlayableCharacter& pg);
+        Level(LevelConfig& config,PlayableCharacter& pg);
         // Given a width, a height and a number of rooms, monsters and items, it creates a new level
 
         void printMap(Point playerPos, Window& mapWindow);
         // Prints the map, the monsters, the items and the player
 
-        void placeCharacter(PlayableCharacter& player, int playerPosition);
+        void placeCharacter(PlayableCharacter& player, pos_pref_t preference);
         // Given a main character and a preference for the position, places the characeter accordingly
         // if playerPosition == 0, the position is random
         // if playerPosition == 1, the position is the same as the one of the down stairs
         // if playerPosition == -1, the position is the same as the one of the up stairs
 
-        int handleTurn(Window& mapWindow, Window& info, Window& bottom, PlayableCharacter& player);
+        status_t handleTurn(Window& mapWindow, Window& info, Window& bottom, PlayableCharacter& player);
         // Handles the player's turn
 
         int getLevel();
@@ -58,10 +70,22 @@ class Level
         // Muove il mostro passato in modo che si avvicini al giocatore 
 
         bool Battle (Window& battle_win, Window& right_win, Window& mapWin, PlayableCharacter& player, Monster& m);
-};
 
-void writeInfo(Window& win,PlayableCharacter& pg, int level);
-//  Scrive le informazioni nella finestra a destra, fornendo i dati su oggetti posseduti e sulle statistiche del PG
+        void showMap(Point pos);
+        // Given the position of the player, it shows the map dinamically: shows the four tiles adjacent to pos, if the
+        // position is not within a room, else it shows the whole room
+
+        status_t pickItUp(PlayableCharacter& player,Window& win);
+        // Makes the player pick up what is beneath him, if possible, printing messages to the given win
+        // Returns 2 if the picked up item was the last special item (which means victory), -2 otherwise
+
+        status_t handleBattles(PlayableCharacter& player, Window& bottom, Window& playerInfo, Window& mapWindow);
+        // Handles the battle with monsters nearby, if present
+        // Returns a LOSS status upon a lost battle, DEFAULT if no battle is lost 
+
+        void writeInfo(Window& win,PlayableCharacter& pg);
+        //  Scrive le informazioni nella finestra a destra, fornendo i dati su oggetti posseduti e sulle statistiche del PG
+};
 
 void writeEquipment(Window& win, PlayableCharacter& pg);
 //  Scrive gli oggetti equipaggiati nella finestra a destra
@@ -69,5 +93,10 @@ void writeEquipment(Window& win, PlayableCharacter& pg);
 int Atk_Def (int def, int atk);
 
 int Luck (int luck);
+
+status_t promptExit(Window& win);
+// Prompts an exit confirmation request on the given window
+// Returns 0 if the exit request is confirmed, -2 otherwise
+
 
 #endif
